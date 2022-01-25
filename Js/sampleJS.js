f60e4d528e4d016e5d9c9459b5dcbe90670095e6
yanshahe <yansha.he@hotmail.com>
2022-01-24 21:50:18 -0600

    buttonJS

---
+++ Js/sampleJS.js
@@ -0,0 +1,10 @@
+function evalNumber() {
+    var inputValue = Number(prompt("Enter any five-digit number without commas"))
+    if (isNaN(inputValue)){
+        alert(inputValue + " is not a number.")
+    }else if (inputValue % 2 == 0){
+        alert(inputValue + " is an even number.")
+    }else{
+        alert(inputValue + " is an odd number.")
+    }
+}
