ed7e207d1a6176df9f8721974b57a261c88991dc
yanshahe <yansha.he@hotmail.com>
2022-01-28 21:17:35 -0600

    js

--- Js/sampleJS.js
+++ Js/sampleJS.js
@@ -8,8 +8,62 @@ function evalNumber() {
         alert(inputValue + " is an odd number.")
     }
 }
-
+// change title
 function changeTitle(){
     let selectedElement = document.getElementById("programCard");
     selectedElement.innerText = "DIGS";
 }
+
+//current minute
+function getminutes(){
+	var d = new Date();
+	var x = document.getElementById("currentminutes");
+	x.innerHTML=d.getMinutes();
+}
+
+//disapper
+function disappearDisplay(){
+    document.getElementById("disappear").style.display="none";
+}
+
+// map
+function mapLoad(){
+    //Define the lat lon coordinate
+    var latLng = [41.789649, -87.599702];
+  
+    var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+  
+    var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
+    streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
+  
+    var map = L.map('map', {
+      center: latLng,
+      zoom: 16,
+      layers: [streets]
+    });
+  
+    var baseLayers = {
+      "Grayscale": grayscale,
+      "Streets": streets
+    };
+  
+    L.control.layers(baseLayers).addTo(map);
+  
+    L.marker(latLng).addTo(map)
+    .bindPopup("<b>UChicago<br>Campus</b>").openPopup();
+  
+  
+  
+    //Click event
+    var popup = L.popup();
+  
+    function onMapClick(e) {
+      popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+    }
+    map.on('click', onMapClick);
+  }
